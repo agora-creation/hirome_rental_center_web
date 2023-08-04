f861a7f506984cf5d6889dd777bd1a245f130be7
Shimamura Yuta <shimamura@agora-c.com>
2023-08-04 11:52:57 +0900

    20230804 commit

--- lib/services/center.dart
+++ lib/services/center.dart
@@ -2,28 +2,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hirome_rental_center_web/models/center.dart';
 
 class CenterService {
-  String _collection = 'center';
-  FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  String collection = 'center';
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   void create(Map<String, dynamic> values) {
-    _firebaseFirestore.collection(_collection).doc(values['id']).set(values);
+    firestore.collection(collection).doc(values['id']).set(values);
   }
 
   void update(Map<String, dynamic> values) {
-    _firebaseFirestore.collection(_collection).doc(values['id']).update(values);
+    firestore.collection(collection).doc(values['id']).update(values);
   }
 
   void delete(Map<String, dynamic> values) {
-    _firebaseFirestore.collection(_collection).doc(values['id']).delete();
+    firestore.collection(collection).doc(values['id']).delete();
   }
 
   Future<CenterModel?> select(String? id) async {
     CenterModel? ret;
-    await _firebaseFirestore
-        .collection(_collection)
-        .doc(id)
-        .get()
-        .then((value) {
+    await firestore.collection(collection).doc(id).get().then((value) {
       ret = CenterModel.fromSnapshot(value);
     });
     return ret;
